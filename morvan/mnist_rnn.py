9fd9e1a82a3a4bb982fc369e817b9ce902a05cd2
potato <fkjtg89@163.com>
2019-01-24 22:07:42 +0800

    Update mnist_rnn.py

--- morvan/mnist_rnn.py
+++ morvan/mnist_rnn.py
@@ -6,16 +6,16 @@ lr = 0.001
 training_iters = 100000
 batch_size = 128
 
-n_inputs = 28
-n_steps = 28
+col_inputs = 28
+row_inputs = 28
 n_hidden_units = 128
 n_classes = 10
 
-x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+x = tf.placeholder(tf.float32, [None, row_inputs, col_inputs])
 y = tf.placeholder(tf.float32, [None, n_classes])
 
 weight = {
-    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
+    'in': tf.Variable(tf.random_normal([col_inputs, n_hidden_units])),
     'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
 }
 
@@ -25,20 +25,22 @@ biases = {
 }
 
 
-def rnn(x, weights, biases):
-    X = tf.reshape(x, [-1, n_inputs])
-
+def rnn(X, weights, biases):
+    # 输入单元的隐藏层X(128batch,28row,28col)
+    # ==>X(128*28,28col)
+    X = tf.reshape(X, [-1, col_inputs])
+    # ==>X_in(128*28,28)
     X_in = tf.matmul(X, weights['in']) + biases['in']
-    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+    # ==>X_in(128,28,128)
+    X_in = tf.reshape(X_in, [-1, row_inputs, n_hidden_units])
 
-    # if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-    #     cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
-    # else:
-    #     cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units)
-    cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units)
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units)
 
-    init_state = cell.zero_state(batch_size, dtype=tf.float32)
-    outputs, final_state = tf.nn.dynamic_rnn(cell, X_in, initial_state=init_state, time_major=False)
+    # lstm_cell划分成2部分（c_state,m_state）
+    init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
+    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=init_state, time_major=False)
+
+    # 输出单元的隐藏层
     outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
     result = tf.matmul(outputs[-1], weights['out'] + biases['out'])
     return result
@@ -59,7 +61,7 @@ with tf.Session() as sess:
 
     while step * batch_size < training_iters:
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+        batch_xs = batch_xs.reshape([batch_size, row_inputs, col_inputs])
         sess.run([train_op], feed_dict={x: batch_xs, y: batch_ys, })
 
         if step % 20 == 0:
