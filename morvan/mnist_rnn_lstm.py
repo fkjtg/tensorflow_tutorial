913dd0a43b3cbd0d397eabbf23d4e8abd7d3f2de
blackpotato <fkjtg89@163.com>
2019-02-13 21:52:33 +0800

    Update mnist_rnn_lstm.py

--- morvan/mnist_rnn_lstm.py
+++ morvan/mnist_rnn_lstm.py
@@ -1,3 +1,5 @@
+'''教程来源：https://morvanzhou.github.io/tutorials/machine-learning/tensorflow/5-08-RNN2/'''
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -8,7 +10,7 @@ batch_size = 128
 
 col_inputs = 28
 row_inputs = 28
-n_hidden_units = 128
+n_hidden_units = 128  # 隐藏层的神经元个数
 n_classes = 10
 
 x = tf.placeholder(tf.float32, [None, row_inputs, col_inputs])
@@ -26,7 +28,16 @@ biases = {
 
 
 def rnn(X, weights, biases):
-    # 输入单元的隐藏层X(128batch,28row,28col)
+    '''
+    RNN 主体结构, 这个 RNN 总共有 3 个组成部分 ( input_layer, cell, output_layer).
+    :param X:
+    :param weights:
+    :param biases:
+    :return:
+    '''
+
+    # 首先，定义input_layer
+    # X(128batch,28row,28col)
     # ==>X(128*28,28col)
     X = tf.reshape(X, [-1, col_inputs])
     # ==>X_in(128*28,28)
@@ -34,15 +45,17 @@ def rnn(X, weights, biases):
     # ==>X_in(128,28,128)
     X_in = tf.reshape(X_in, [-1, row_inputs, n_hidden_units])
 
+    # 接着，cell中计算。state划分成2部分（c_state,m_state）
     lstm_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units)
-
-    # lstm_cell划分成2部分（c_state,m_state）
     init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
     outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=init_state, time_major=False)
 
-    # 输出单元的隐藏层
-    outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
-    result = tf.matmul(outputs[-1], weights['out'] + biases['out'])
+    # 最后，output_layer和result的值
+    # 方式一输出结果
+    result = tf.matmul(final_state[1], weights['out'] + biases['out'])
+    # # 方式二输出结果
+    # outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
+    # result = tf.matmul(outputs[-1], weights['out'] + biases['out'])
     return result
 
 
